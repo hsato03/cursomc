a71655630190de28994c8fc6146789504866d3d5
Henrique Sato <henriquesato2003@gmail.com>
2022-11-13 12:54:12 -0300

    Produto e associacao muitos para muitos

---
+++ backend/src/main/java/com/project/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.project.cursomc.repositories;
+
+import com.project.cursomc.entities.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
