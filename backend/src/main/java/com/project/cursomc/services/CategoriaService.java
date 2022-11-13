34dfea74093a72048cbe5441ddfb6f8734475129
Henrique Sato <henriquesato2003@gmail.com>
2022-11-13 18:03:36 -0300

    Ajustes no endpoint /categorias/{id}

--- backend/src/main/java/com/project/cursomc/services/CategoriaService.java
+++ backend/src/main/java/com/project/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.project.cursomc.services;
 
 import com.project.cursomc.entities.Categoria;
 import com.project.cursomc.repositories.CategoriaRepository;
+import com.project.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,8 @@ public class CategoriaService {
     @Transactional(readOnly = true)
     public Categoria findById(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! " +
+                "Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
 }
