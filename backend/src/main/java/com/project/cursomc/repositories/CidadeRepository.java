beb6743093a8d792657f9f04b6b1f527e40e1b1d
Henrique Sato <henriquesato2003@gmail.com>
2022-11-13 18:41:32 -0300

    Estado e cidade

---
+++ backend/src/main/java/com/project/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.project.cursomc.repositories;
+
+import com.project.cursomc.entities.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+}
