b1b2173b13e4fcdf9d302ac518f9fd5484951a4c
Henrique Sato <henriquesato2003@gmail.com>
2022-11-13 23:24:32 -0300

    Cliente, TipoCliente, telefones e enderecos

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -18,3 +18,10 @@ INSERT INTO CIDADE(nome, estado_id) VALUES ('Uberlândia', 1);
 INSERT INTO CIDADE(nome, estado_id) VALUES ('São Paulo', 2);
 INSERT INTO CIDADE(nome, estado_id) VALUES ('Campinas', 2);
 
+INSERT INTO CLIENTE(nome, email, cpf_ou_cnpj, tipo) VALUES ('Maria Silva', 'maria@gmail.com', '36478912377', 1);
+
+INSERT INTO TELEFONE(cliente_id, telefones) VALUES (1, '27363323');
+INSERT INTO TELEFONE(cliente_id, telefones) VALUES (1, '93838393');
+
+INSERT INTO ENDERECO(logradouro, numero, complemento, bairro, cep, cliente_id, cidade_id) VALUES ('Rua Flores', '300', 'Apto 203', 'Jardim', '38220634', 1, 1);
+INSERT INTO ENDERECO(logradouro, numero, complemento, bairro, cep, cliente_id, cidade_id) VALUES ('Avenida Matos', '105', 'Sala 800', 'Centro', '38777012', 1, 2);
